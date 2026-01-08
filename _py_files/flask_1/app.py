115510f909b4e203a6731f7bb34519fecda560cd
robteach <robert.cleary.teach@gmail.com>
2026-01-08 21:42:14 +0000

    update latest

--- _py_files/flask_1/app.py
+++ _py_files/flask_1/app.py
@@ -1,11 +1,15 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == "__main__": #if you execute this file
     app.run(debug=True)
