673ed796a5fda2b9e8a65ee7207491a8308d1cb3
robteach <robert.cleary.teach@gmail.com>
2026-01-15 17:31:09 +0000

    update latest examples

--- _py_files/flask_3/app.py
+++ _py_files/flask_3/app.py
@@ -1,11 +1,27 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, session
 
 app = Flask(__name__)
+app.secret_key =  'put a real secret key here: this just for demo'
 city_data = [
      {"id":1, "name": "New York", "latitude": 40.7128, "longitude": -74.0060}
     ,{"id":2, "name": "Los Angeles","latitude": 34.0522,"longitude": -118.2437}
 ]
+
+
+
+@app.route('/')
+def home():                                                     	#remove name=None parameter
+    print(session)
+    a_name = session.get("name", None)                            	#default to None if not exists
+    return render_template("index.html", name=a_name)        	    #pass data to Jinja templating
+
+@app.route('/greet')
+@app.route('/greet/<a_name>')
+def greet(a_name=None):
+        session['name'] = a_name
+        return redirect( url_for('home') )
+    
 
 
 @app.route("/visit/<int:city_id>")                      #CHANGE: add 'int:' before a_num
@@ -18,17 +34,6 @@ def visit(city_id):
          if city["id"] == city_id:
               city_name = city["name"]
     return f"<h1> Enjoy your visit to: {city_name} </h1>"
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/greet')
-@app.route('/greet/<a_name>')
-def greet(a_name=""):
-        return render_template('greet.html', name=a_name)
-    
-
 
 
 if __name__ == "__main__": #if you execute this file
