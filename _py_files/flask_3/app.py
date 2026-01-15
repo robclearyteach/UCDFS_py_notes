94bfb60e1f143e2b1401bdbece3eb94ec47a28b7
robteach <robert.cleary.teach@gmail.com>
2026-01-15 18:39:27 +0000

    update flask_3/app.py, minimilised version to begin with

--- _py_files/flask_3/app.py
+++ _py_files/flask_3/app.py
@@ -1,12 +1,6 @@
 
-from flask import Flask, render_template, url_for, redirect, session
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
-app.secret_key =  'put a real secret key here: this just for demo'
-city_data = [
-     {"id":1, "name": "New York", "latitude": 40.7128, "longitude": -74.0060}
-    ,{"id":2, "name": "Los Angeles","latitude": 34.0522,"longitude": -118.2437}
-]
 
 
 
@@ -22,18 +16,6 @@ def greet(a_name=None):
         session['name'] = a_name
         return redirect( url_for('home') )
     
-
-
-@app.route("/visit/<int:city_id>")                      #CHANGE: add 'int:' before a_num
-def visit(city_id):    
-    #python code to compute digit sum of a_num
-    print(f"visit(): {city_id=}")
-    city_name=None                                      #bug-fix: renamed: distinct to loop 'city'
-    for city in city_data:
-         print(f"inside for: {city=}, {city["id"]=}, {city["name"]=}")
-         if city["id"] == city_id:
-              city_name = city["name"]
-    return f"<h1> Enjoy your visit to: {city_name} </h1>"
 
 
 if __name__ == "__main__": #if you execute this file
